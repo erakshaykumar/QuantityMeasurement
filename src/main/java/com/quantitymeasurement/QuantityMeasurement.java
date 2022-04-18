dbecaacdf3e1064e237eca68392054471205b006
erakshaykumar <akaakshay304@gmail.com>
2022-04-18 17:01:07 +0530

    Added: Compare Unit Logic For Feet And Inch

--- src/main/java/com/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -1,5 +1,15 @@
+/* @Purpose: As a math student, I wish to compare lengths 1ft = 12in 
+ * @File: Quantity Measurement 
+ * @Author: Akshay Kumar
+ */
+
 package com.quantitymeasurement;
 
+/*
+ * @Purpose: Compare units 1feet = 12inch logic 
+ * @Param: double Feet , double Inch
+ * @Result: Boolean (true,false) & Welcome Message
+ */
 public class QuantityMeasurement {
 	public boolean compareUnits(double feet, double inch) {
 		if (inch == 12 * feet)
