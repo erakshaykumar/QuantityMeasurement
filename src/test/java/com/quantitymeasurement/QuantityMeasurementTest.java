37ba35775041ca8cfc210f6e5f53830b0a425d52
erakshaykumar <akaakshay304@gmail.com>
2022-04-18 16:49:50 +0530

    Added: Compare Units For Feet And Inches

--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,16 +1,10 @@
 package com.quantitymeasurement;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
-	private QuantityMeasurement quantitymeasurement;
-
-	@Before
-	public void init() {
-		quantitymeasurement = new QuantityMeasurement();
-	}
+	QuantityMeasurement quantitymeasurement = new QuantityMeasurement();
 
 	@Test
 	public void givenFeetAndInchesValue_WhenEqual_ShouldReturnTrue() {
